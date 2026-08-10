9a1815dacf48c44acbc9a7e879ebd5565bf16474
Mirko D'Addiego <mirkodaddiego@gmail.com>
2026-08-10 20:22:40 +0200

    chore(frontend-backoffice): setup vitest, testing-library, proxy vite verso il backend

--- frontend-backoffice/vite.config.ts
+++ frontend-backoffice/vite.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
-  }
+    host: true,
+    proxy: {
+      '/auth': 'http://localhost:3000',
+      '/backoffice': 'http://localhost:3000',
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/testUtil/setup.ts'],
+    globals: true,
+  },
 });
